2245b6b7286ead26f8507ca24a7a178eae33b0c0
Tibira Botto <tibira.botto@gmail.com>
2023-11-23 14:20:34 -0500

    feature: updateprofile

--- db/queries/users.js
+++ db/queries/users.js
@@ -22,11 +22,33 @@ const addUser = async(data) => {
   });
 };
 
+const updateUser = async(data, username) => {
+  const { fullName, password, email } = data;
+  const sql = `UPDATE "users" SET "full_name" = $1, "email" = $2, "password" = $3 WHERE "username" = $4;`;
+  const passwordHashed = await bcrypt.hash(password, 10);
+  const values = [fullName, email, passwordHashed, username];
+  return db.query(sql, values).then((data) => {
+    if (data.rows) {
+      return true;
+    } else {
+      return false;
+    }
+  }).catch((e) => {
+    console.log(e);
+  });
+};
+
 const findByEmail = async(email) => {
   const sql = "SELECT username FROM users WHERE email = $1";
   const values = [email];
   return (await db.query(sql, values)).rows[0].username;
 };
 
-module.exports = { getUsers, addUser, findByEmail };
+const findByUsername = async(username) => {
+  const sql = "SELECT * FROM users WHERE username = $1";
+  const values = [username];
+  return (await db.query(sql, values)).rows[0];
+};
+
+module.exports = { getUsers, addUser, findByEmail,findByUsername, updateUser };
 
