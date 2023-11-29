5623bfc37cc0eb3c9c5ec6c0684a9fc2edb3fe2d
Urvish Prajapati <urvishprajapati412@gmail.com>
2023-11-29 17:15:28 -0500

    seeds changed

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,6 +1,6 @@
-INSERT INTO users (username, full_name, email, password) VALUES ('John', 'John Doe', 'johndoe@gmail.com', '123');
+INSERT INTO users (username, full_name, email, password) VALUES ('John', 'John Doe', 'johndoe@gmail.com', '$2b$10$MpRbX2lBQcimqIb15stuUemPZCtlRbX7aTMapSb4ig2IzSCfIt9p.');
 INSERT INTO users (username, full_name, email, password) VALUES ('Amy', 'Amy Jackson', 'amyjack@gmail.com', '123');
 INSERT INTO users (username, full_name, email, password) VALUES ('Christy', 'Christina Diaz', 'chris@gmail.com', '123');
 INSERT INTO users (username, full_name, email, password) VALUES ('Mark', 'Mark Maltese', 'mark@gmail.com', '123');
-INSERT INTO users (username, full_name, email, password) VALUES ('Uvprjp36', 'Urvish Prajapati', 'uv@gmail.com', '123');
+INSERT INTO users (username, full_name, email, password) VALUES ('Uvprjp36', 'Urvish Prajapati', 'uv@gmail.com', '$2b$10$MpRbX2lBQcimqIb15stuUemPZCtlRbX7aTMapSb4ig2IzSCfIt9p.');
 INSERT INTO users (username, full_name, email, password) VALUES ('Libin', 'Libin Ali', 'libinali@gmail.com', '123');
